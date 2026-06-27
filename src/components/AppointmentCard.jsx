58ebf88bfe660fbcb790d8f6f41ef5ed19df6214
mahdiyeh <mahdiyehhaj497@gmail.com>
2026-06-27 12:12:32 +0330

    build Appontmentcard

--- src/components/AppointmentCard.jsx
+++ src/components/AppointmentCard.jsx
@@ -1,6 +1,14 @@
-export default function AppointmentCard() {
+export default function AppointmentCard({appointment , onDelete}) {
     return (
         <>
-        <div>Apponintment Card</div></>
+            <div className="border p-3 rounded mb-2">
+                <h4 className="font-bold ">{appointment.doctor }</h4>
+                <p> Date : {appointment.date }</p>
+                <p> time :{appointment.time }</p>
+                <button onClick={() => onDelete(appointment.id)}
+                className="mt-2 bg-red-600 text-white px-3 py-1 rounded text=-sm"
+                >Delete</button>
+            </div>
+        </>
     )
 }
