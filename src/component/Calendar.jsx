6c33d715be4771a165c3137fc0022f236e250ea5
mahdiyeh <mahdiyehhaj497@gmail.com>
2026-06-30 12:44:31 +0330

    build calender

--- src/component/Calendar.jsx
+++ src/component/Calendar.jsx
@@ -1,7 +1,17 @@
-export default function Calendar() {
+export default function Calendar({days, onSelectDay}) {
     return (
         <>
-        <div>calendar</div>
+            <div>
+                {days.map((day) => (
+                    <button
+                        key={day}
+                        onClick={() => onSelectDay(day)}
+                        className="p-3 bg-gray-100 rounded-lg hover:bg-blue-500 hover:text-white transation"
+                    > 
+                        {new Date(day).toLocaleDateString("fa-IR")}
+                    </button>
+                ))}
+        </div>
         </>
     )
 }
