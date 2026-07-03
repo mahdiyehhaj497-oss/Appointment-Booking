d11ebaee9eaf7474d2cedaa96a3b6b0159e02203
mahdiyeh <mahdiyehhaj497@gmail.com>
2026-07-03 18:45:58 +0330

    build important components

--- src/component/TimeSlots.jsx
+++ src/component/TimeSlots.jsx
@@ -1,7 +1,15 @@
-export default function TimeSlots() {
+export default function TimeSlots({slots,onSelectSlot}) {
     return (
         <>
-        <div>Timeslots</div>
+            <div className="grid grid-cols-3 gap-3 mt-4">
+                {slots.map((slot) => (
+                    <button
+                    key={slot}
+                        onClick={() => onSelectSlot(slot)}
+                        className="p-2 bg-green-100 rounded-lg hover:bg-green-500 hover:text-white transition "
+                    >{slot }</button>
+                ))}
+        </div>
         </>
     )
 }
