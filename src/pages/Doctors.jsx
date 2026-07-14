fcc040248b12d29579985393e7f10087a070ea1f
mahdiyeh <mahdiyehhaj497@gmail.com>
2026-07-14 15:18:51 +0330

    ...

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom"
 
 export default function Doctors() {
@@ -10,17 +10,18 @@ export default function Doctors() {
             id: 1,
             name: "دکتر سارا احمدی",
             specialty: "قلب وعروق",
-            image: "",
+            image: "src/assets/pexels-tima-miroshnichenko-5407222 (1).jpg",
             availableDays: ["2026-06-22", "2026-06-23"],
             timeSlots: ["09:00", "10:00", "11:00", "14:00"]
         }, {
             id: 2,
             name: "دکتر رضا محمدی",
             specialty: "ارتوپد",
-            image: "",
+            image: "src/assets/pexels-cottonbro-5867737.jpg",
             availableDays: ["2026-06-24", "2026-06-26"],
             timeSlots: ["8:00, 9:30, 13:00"]
-            }];
+        }];
+        
         setDoctors(data)
         
     }, [])
